120908a2f7bc933409fd2e2edabc8d18ea3f01c6
Mihika <67188124+mihikaj2001@users.noreply.github.com>
2021-12-08 22:42:53 +0530

    Update db.sql

--- templates/db.sql
+++ templates/db.sql
@@ -8,11 +8,17 @@ CREATE TABLE `tbl_user`
 
     `user_name` varchar(45) NOT NULL,
 
-    `phn_no` varchar(10) NOT NULL,  
+    `phn_no` varchar(15) NOT NULL,  
 
     `user_email` varchar(45) NOT NULL,
 
     `user_password` varchar(255) NOT NULL,
+ 
+    user_address varchar(400) DEFAULT NULL,
+    
+    user_city varchar(50) DEFAULT NULL,
+ 
+    user_zipcode varchar(15) DEFAULT NULL,
 
 PRIMARY KEY (`user_id`) );
 
