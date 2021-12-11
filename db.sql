5145223067d7d24264cca1e7d59dd766231229f9
Mihika <67188124+mihikaj2001@users.noreply.github.com>
2021-12-11 10:34:38 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -10,7 +10,18 @@ CREATE TABLE `product` (
     quantity int,
     category varchar(40),
 	PRIMARY KEY (`id`)
-)
+);
+
+-- products insert
+INSERT INTO `product` (`id`, `name`, `code`, `image`, `price`, `quantity`, `category` ) VALUES
+(1, 'American Tourist', 'AMTR01', 'product-images/bag.jpg', 12000.00, 1000, 'bags'),
+(2, 'EXP Portable Hard Drive', 'USB02', 'product-images/external-hard-drive.jpg', 5000.00, 20, 'tech'),
+(3, 'Shoes', 'SH03', 'product-images/shoes.jpg', 1000.00, 30, 'shoes'),
+(4, 'XP 1155 Intel Core Laptop', 'LPN4', 'product-images/laptop.jpg', 80000.00, 5, 'tech'),
+(5, 'FinePix Pro2 3D Camera', '3DCAM01', 'product-images/camera.jpg', 150000.00),
+(6, 'Simple Mobile', 'MB06', 'product-images/mobile.jpg', 3000.00),
+(7, 'Luxury Ultra thin Wrist Watch', 'WristWear03', 'product-images/watch.jpg', 3000.00),
+(8, 'Headphone', 'HD08', 'product-images/headphone.jpg', 400.00);
 
 
 -- Create table 'tbl_user'
