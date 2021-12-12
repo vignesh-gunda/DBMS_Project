84e954a7ed901e8fa0ec8b73f982d98b7c2aaa79
Drishti981 <89929088+Drishti981@users.noreply.github.com>
2021-12-12 15:56:35 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -14,37 +14,49 @@ CREATE TABLE `product`(
 
 -- products insert
 
-INSERT INTO `product` (`id`, `name`, `code`, `image`, `price`, quantity, category)
+ INSERT INTO `products` (`id`, `name`, `code`, `image`, `price`, quantity, category, merchant_id)
 VALUES
- (1, 'red dress', 'RDDR', 'product-images/dress.jpg', 4200.00, 30, 'clothes'),
+ (1, 'red dress', 'RDDR', 'product-images/dress.jpg', 4200.00, 30, 'clothes', 'drishti@gmail.com'),
 
- (2, 'spl aviator-sunglasses', 'SASG', 'product-images/aviator-sunglasses.jpg', 2900.00, 20, 'accessories'),
+ (2, 'spl aviator-sunglasses', 'SASG', 'product-images/aviator-sunglasses.jpg', 2900.00, 20, 'accessories', 'mihika@gmail.com'),
 
- (3, 'Shoes', 'SH01', 'product-images/shoes.jpg', 1000.00, 40, 'shoes'),
+ (3, 'Shoes', 'SH01', 'product-images/shoes.jpg', 1000.00, 40, 'shoes', 'drishti@gmail.com'),
 
- (4, 'hot pink scarf', 'HPS', 'product-images/scarf.jpg', 400.00, 40, 'clothes'),
+ (4, 'hot pink scarf', 'HPS', 'product-images/scarf.jpg', 400.00, 40, 'clothes', 'vignesh@gmail.com'),
 
- (5, 'set of 3 ties', '3TS01', 'product-images/ties.jpg', 1200.00, 60, 'accessories'),
+ (5, 'set of 3 ties', '3TS01', 'product-images/ties.jpg', 1200.00, 60, 'accessories', 'vignesh@gmail.com'),
 
- (6, 'Simple wallet xd', 'WLT06', 'product-images/wallets.jpg', 1500.00, 50, 'accessories'),
+ (6, 'Simple wallet xd', 'WLT06', 'product-images/wallets.jpg', 1500.00, 50, 'accessories', 'drishti@gmail.com'),
 
- (7, 'LU1V handbag', 'IV04', 'product-images/handbag.jpg', 12000.00, 8, 'accessories'),
+ (7, 'LU1V handbag', 'IV04', 'product-images/handbag.jpg', 12000.00, 8, 'accessories', 'mihika@gmail.com'),
 
- (8, 'UXI bandana', 'UXB', 'product-images/bandanas.jpg', 400.00, 10, 'accessories'),
+ (8, 'UXI bandana', 'UXB', 'product-images/bandanas.jpg', 400.00, 10, 'accessories', 'drishti@gmail.com'),
 
- (9, 'CLK gloves', 'CGLK', 'product-images/gloves.jpg', 1400.00, 60, 'accessories'),
+ (9, 'CLK gloves', 'CGLK', 'product-images/gloves.jpg', 1400.00, 60, 'accessories', 'vignesh@gmail.com'),
 
- (10, 'multicoloured silk dress', 'MCSLK', 'product-images/mcsilkdress.jpg', 5400.00, 40, 'clothes'),
+ (10, 'multicoloured silk dress', 'MCSLK', 'product-images/mcsilkdress.jpg', 5400.00, 40, 'clothes', 'vignesh@gmail.com'),
 
- (11, 'heels', 'HL004', 'product-images/heels.jpg', 6400.00, 8, 'accessories'),
+ (11, 'heels', 'HL004', 'product-images/heels.jpg', 6400.00, 8, 'accessories', 'drishti@gmail.com'),
 
- (12, 'wedges', 'WHL02', 'product-images/wedges.jpg', 5600.00, 8, 'accessories'),
+ (12, 'wedges', 'WHL02', 'product-images/wedges.jpg', 5600.00, 8, 'accessories', 'vignesh@gmail.com'),
 
- (13, 'Denim jacket', 'DJKT1', 'product-images/denimjacket.jpg', 4400.00, 40, 'clothes'),
+ (13, 'Denim jacket', 'DJKT1', 'product-images/denimjacket.jpg', 4400.00, 40, 'clothes', 'mihika@gmail.com'),
 
- (14, 'Denim jacket', 'DJKT2', 'product-images/denimjacket2.jpg', 4400.00, 40, 'clothes'),
+ (14, 'Denim jacket', 'DJKT2', 'product-images/denimjacket2.jpg', 4400.00, 40, 'clothes', 'mihika@gmail.com'),
 
- (15, 'UNX Boots', 'BO08', 'product-images/boots.jpg', 7800.00, 20, 'shoes');
+ (15, 'UNX Boots', 'BO08', 'product-images/boots.jpg', 7800.00, 20, 'shoes', 'vignesh@gmail.com'),
+
+ (16, 'Blue Print Dress', 'BPD7', 'product-images/D2.jpg', 2500.00, 20, 'clothes', 'drishti@gmail.com'),
+
+ (17, 'Black Dress', 'LBD19', 'product-images/D3.jpg', 2350.00, 20, 'clothes', 'vignesh@gmail.com'),
+
+ (18, 'White Dress', 'DW440', 'product-images/D4.jpg', 2100.00, 20, 'clothes', 'mihika@gmail.com'),
+
+ (19, 'Red Coat', 'REDC', 'product-images/C2.jpg', 3700.00, 20, 'clothes', 'drishti@gmail.com'),
+
+ (20, 'White Coat', 'WH12', 'product-images/C1.jpg', 4300.00, 20, 'clothes', 'mihika@gmail.com');
+
+
 -- Create table 'tbl_user'
 
 CREATE TABLE `tbl_user`
